b4749411a9d380e7e5f5648b0548105d313d865b
Bob Cousins <bobcousins42@googlemail.com>
2013-09-29 00:21:42 +0100

    RAMPS-FD-EXT; routing and gerbers

---
+++ RAMPS-FD-EXT/gerbers/makezip.bat
@@ -0,0 +1,21 @@
+@echo off
+rem set zzzname=%1 
+set zzzname=RAMPS-FD-EXT
+
+copy %zzzname%.drl %zzzname%.txt
+
+del %zzzname%.zip
+pkzip -add -silent %zzzname%.zip  %zzzname%-Front.GTL
+pkzip -add -silent %zzzname%.zip  %zzzname%-Back.GBL
+pkzip -add -silent %zzzname%.zip  %zzzname%-F_Mask.GTS
+pkzip -add -silent %zzzname%.zip  %zzzname%-B_Mask.GBS
+pkzip -add -silent %zzzname%.zip  %zzzname%-F_SilkS.GTO
+pkzip -add -silent %zzzname%.zip  %zzzname%-B_SilkS.GBO
+pkzip -add -silent %zzzname%.zip  %zzzname%-Edge_Cuts.GBR
+pkzip -add -silent %zzzname%.zip  %zzzname%.TXT
+
+
+set zzzname=
+
+
+
